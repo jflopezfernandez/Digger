434aeb02fad1ba00ba05ca52ebcf7694b2397a3c
lopezfjose <jflopezfernandez@gmail.com>
2018-11-07 21:14:24 -0500

    Stopped to update visual studio

--- digger/Digger.cpp
+++ digger/Digger.cpp
@@ -1,10 +1,32 @@
 
-#include <iostream>
+#include "Digger.h"
+
+using Int = __int64;
+
+extern "C" Int Int64Add(Int a, Int b, Int c);
+
+namespace Digger
+{
+    template <typename T>
+    void PrintItem(const std::string& str, T arg)
+    {
+        std::cout << str << ": " << arg << '\n';
+    }
+}
 
 
 int main()
 {
-    std::cout << "<Testing...>" << '\n';
+    const Int a = 72;
+    const Int b = 8;
+    const Int c = 3;
 
-    return 0;
+    const auto s = Int64Add(a, b, c);
+
+    Digger::PrintItem("a", a);
+    Digger::PrintItem("b", b);
+    Digger::PrintItem("c", c);
+    Digger::PrintItem("s", s);
+
+    return EXIT_SUCCESS;
 }
--- digger/Digger.h
+++ digger/Digger.h
@@ -1,6 +1,8 @@
 
 #pragma once
 
+#include <Windows.h>
+
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>

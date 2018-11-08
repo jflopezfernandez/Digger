2805adea1046f712d7d727b5aa7e5b282227f624
lopezfjose <jflopezfernandez@gmail.com>
2018-11-07 20:31:27 -0500

    Initial commit

---
+++ digger/Digger.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "<Testing...>" << '\n';
+
+    return 0;
+}
---
+++ digger/Digger.h
@@ -0,0 +1,50 @@
+
+#pragma once
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cstddef>
+#include <cassert>
+#include <cstdarg>
+#include <climits>
+#include <cinttypes>
+#include <cmath>
+#include <ctime>
+
+#include <algorithm>
+#include <any>
+#include <array>
+#include <chrono>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <filesystem>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <locale>
+#include <memory>
+#include <mutex>
+#include <new>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <ratio>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <string_view>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <type_traits>
+#include <typeinfo>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#include <gsl/gsl>
